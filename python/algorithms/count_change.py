d0b794c805d1fc39e9037d837dd9f6b3a06f8146
DevCorvus <devcorvus.contact@gmail.com>
2023-11-07 21:38:28 -0400

    rename "counting change" to "count change" in python algorithms

--- python/algorithms/count_change.py
+++ python/algorithms/count_change.py
@@ -1,7 +1,7 @@
 import time
 
 
-def counting_change(amount, coins, coinIndex=0):
+def count_change(amount, coins, coinIndex=0):
     if amount == 0:
         return 1
 
@@ -15,14 +15,14 @@ def counting_change(amount, coins, coinIndex=0):
     qty = 0
     while qty * value <= amount:
         sub_amount = amount - (qty * value)
-        total_ways += counting_change(sub_amount, coins, coinIndex + 1)
+        total_ways += count_change(sub_amount, coins, coinIndex + 1)
 
         qty += 1
 
     return total_ways
 
 
-def counting_change_with_memo(amount, coins, coinIndex=0, memo={}):
+def count_change_with_memo(amount, coins, coinIndex=0, memo={}):
     if amount == 0:
         return 1
 
@@ -40,7 +40,7 @@ def counting_change_with_memo(amount, coins, coinIndex=0, memo={}):
     qty = 0
     while qty * value <= amount:
         sub_amount = amount - (qty * value)
-        total_ways += counting_change_with_memo(sub_amount, coins, coinIndex + 1, memo)
+        total_ways += count_change_with_memo(sub_amount, coins, coinIndex + 1, memo)
 
         qty += 1
 
@@ -52,9 +52,9 @@ amount = 4
 coins = [1, 2, 3]
 
 t = time.process_time()
-print("No Memo:", counting_change(amount, coins))
+print("No Memo:", count_change(amount, coins))
 print("took {:f} seconds".format(time.process_time() - t))
 
 t = time.process_time()
-print("Memoized:", counting_change_with_memo(amount, coins, memo={}))
+print("Memoized:", count_change_with_memo(amount, coins, memo={}))
 print("took {:f} seconds".format(time.process_time() - t))
