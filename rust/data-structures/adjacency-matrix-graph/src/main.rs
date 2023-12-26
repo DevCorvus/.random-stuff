5a7db040f12e912ae6bc33e38618dd4ba75111c2
DevCorvus <devcorvus.contact@gmail.com>
2023-12-26 13:19:28 -0400

    add "breadth first search" algorithm in adjacency matrix graph

--- rust/data-structures/adjacency-matrix-graph/src/main.rs
+++ rust/data-structures/adjacency-matrix-graph/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::VecDeque;
+
 struct AdjacencyMatrixGraph {
     data: Vec<Vec<usize>>,
     size: usize,
@@ -45,20 +47,40 @@ impl AdjacencyMatrixGraph {
             }
         }
     }
+
+    fn breadth_first_search(&self, start: usize) {
+        let mut visited = vec![false; self.size];
+        let mut queue = VecDeque::new();
+
+        visited[start] = true;
+        queue.push_back(start);
+
+        while let Some(i) = queue.pop_front() {
+            println!("{}", i);
+            for (next, weight) in self.data[i].iter().enumerate() {
+                if *weight != 0 && !visited[next] {
+                    visited[next] = true;
+                    queue.push_back(next);
+                }
+            }
+        }
+    }
 }
 
 fn main() {
     let mut graph = AdjacencyMatrixGraph::new(4);
 
-    graph.add_directed_edge(0, 1, 1);
-    graph.add_directed_edge(1, 3, 1);
-    graph.add_directed_edge(3, 2, 1);
-    graph.add_directed_edge(2, 1, 1);
-    graph.add_directed_edge(2, 3, 1);
+    graph.add_undirected_unweighted_edge(0, 1);
+    graph.add_undirected_unweighted_edge(0, 2);
+    graph.add_undirected_unweighted_edge(1, 3);
 
     for row in &graph.data {
         println!("{:?}", row);
     }
 
-    graph.depth_first_search(3);
+    println!("DFS");
+    graph.depth_first_search(0);
+
+    println!("BFS");
+    graph.breadth_first_search(0);
 }
