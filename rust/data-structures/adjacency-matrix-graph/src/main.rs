0d879c7be413651ab40569ca143b134da5208abb
DevCorvus <devcorvus.contact@gmail.com>
2023-12-26 10:58:00 -0400

    add "depth first search" algorithm in adjacency matrix graph

--- rust/data-structures/adjacency-matrix-graph/src/main.rs
+++ rust/data-structures/adjacency-matrix-graph/src/main.rs
@@ -25,14 +25,40 @@ impl AdjacencyMatrixGraph {
     fn add_undirected_unweighted_edge(&mut self, from: usize, to: usize) {
         self.add_undirected_edge(from, to, 1);
     }
+
+    fn depth_first_search(&self, start: usize) {
+        let mut visited = vec![false; self.size];
+        self._depth_first_search(start, &mut visited);
+    }
+
+    fn _depth_first_search(&self, at: usize, visited: &mut Vec<bool>) {
+        if visited[at] {
+            return;
+        }
+
+        visited[at] = true;
+        println!("{}", at);
+
+        for (i, weight) in self.data[at].iter().enumerate() {
+            if *weight != 0 {
+                self._depth_first_search(i, visited);
+            }
+        }
+    }
 }
 
 fn main() {
     let mut graph = AdjacencyMatrixGraph::new(4);
 
-    graph.add_undirected_unweighted_edge(0, 1);
+    graph.add_directed_edge(0, 1, 1);
+    graph.add_directed_edge(1, 3, 1);
+    graph.add_directed_edge(3, 2, 1);
+    graph.add_directed_edge(2, 1, 1);
+    graph.add_directed_edge(2, 3, 1);
 
-    for row in graph.data {
+    for row in &graph.data {
         println!("{:?}", row);
     }
+
+    graph.depth_first_search(3);
 }
