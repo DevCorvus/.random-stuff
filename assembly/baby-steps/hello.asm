326237f737cb6ea4d34f833965c0fa744fba940b
DevCorvus <devcorvus.contact@gmail.com>
2024-03-22 16:29:51 -0400

    move strlen and print functions to another file in assembly program

--- assembly/baby-steps/hello.asm
+++ assembly/baby-steps/hello.asm
@@ -3,6 +3,8 @@ format ELF64 executable 3
 segment readable executable
 entry main
 
+include 'io.asm'
+
 main:
     lea rdi, [msg]  ; address of msg into rdi
     call print      ; print msg
@@ -11,29 +13,6 @@ main:
     xor rdi, rdi    ; exit code 0
     mov rax, 60     ; sys_exit
     syscall
-
-strlen:
-    push rdi        ; push to stack
-    push rcx        ; push to stack (this is our counter)
-    mov rcx, -1     ; set rcx to -1
-    xor al, al      ; set al to 0
-    cld             ; clear direction flags
-    repne scasb     ; repeat if not equal to al (0)
-    neg rcx         ; negate rcx (now it's a positive number)
-    dec rcx         ; exclude null terminator
-    mov rax, rcx    ; move value from rcx to rax
-    pop rcx         ; restore rcx
-    pop rdi         ; restore rdi
-    ret             ; return
-
-print:
-    call strlen     ; calculate msg length
-    mov rdx, rax    ; move rax to rdx
-    mov rsi, rdi    ; move rdi to rsi
-    mov rdi, 1      ; stdout
-    mov rax, 1      ; sys_write
-    syscall
-    ret
 
 segment readable writable
 
---
+++ assembly/baby-steps/io.asm
@@ -0,0 +1,22 @@
+strlen:
+    push rdi        ; push to stack
+    push rcx        ; push to stack (this is our counter)
+    mov rcx, -1     ; set rcx to -1
+    xor al, al      ; set al to 0
+    cld             ; clear direction flags
+    repne scasb     ; repeat if not equal to al (0)
+    neg rcx         ; negate rcx (now it's a positive number)
+    dec rcx         ; exclude null terminator
+    mov rax, rcx    ; move value from rcx to rax
+    pop rcx         ; restore rcx
+    pop rdi         ; restore rdi
+    ret             ; return
+
+print:
+    call strlen     ; calculate msg length
+    mov rdx, rax    ; move rax to rdx
+    mov rsi, rdi    ; move rdi to rsi
+    mov rdi, 1      ; stdout
+    mov rax, 1      ; sys_write
+    syscall
+    ret
