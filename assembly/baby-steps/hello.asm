c791c00cc4a01d48824354080081961f6a586c52
DevCorvus <devcorvus.contact@gmail.com>
2024-03-22 17:58:35 -0400

    add read stdin function to assembly hello world program

--- assembly/baby-steps/hello.asm
+++ assembly/baby-steps/hello.asm
@@ -6,15 +6,21 @@ entry main
 include 'io.asm'
 
 main:
-    lea rdi, [msg]  ; address of msg into rdi
-    call print      ; print msg
-    lea rdi, [name] ; address of name into rdi
-    call print      ; print name
-    xor rdi, rdi    ; exit code 0
-    mov rax, 60     ; sys_exit
+    lea     rdi, [msg]      ; address of msg into rdi
+    call    print           ; print msg
+    lea     rdi, [prompt]   ; address of prompt into rdi
+    call    print           ; print prompt
+    lea     rsi, [buf]      ; buffer address into rsi
+    mov     rdi, 64         ; buffer size
+    call    read            ; read user input
+    mov     rdi, rsi        ; move string read to rdi
+    call    print           ; print buffer (user input)
+    xor     rdi, rdi        ; exit code 0
+    mov     rax, 60         ; sys_exit
     syscall
 
 segment readable writable
 
-msg db 'Hello world', 10, 0 ; Hello world message + \n + \0 (13 bytes)
-name db 'Luis Portillo', 10, 0
+msg     db 'Hello world', 10, 0 ; Hello world message + \n + \0 (13 bytes)
+prompt  db 'Enter your name: ', 0
+buf     rb 64
